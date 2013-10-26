7e26a9ef28f02f794772c5e4567c5707c24ca25e
ithillel-areks <areksfrai@ya.ru>
2013-10-26 18:30:37 +0300

    No idea what I'm doing.

--- Xipiv/src/org/hillel/it/xipiv/model/entity/Image.java
+++ Xipiv/src/org/hillel/it/xipiv/model/entity/Image.java
@@ -1,5 +1,9 @@
 package org.hillel.it.xipiv.model.entity;
 
+import java.util.*;
+
 public class Image {
 	String imageTitle;
+	List<String> comments = new ArrayList<String>();
+	int rating; // One to five
 }
--- Xipiv/src/org/hillel/it/xipiv/model/entity/User.java
+++ Xipiv/src/org/hillel/it/xipiv/model/entity/User.java
@@ -3,8 +3,12 @@ package org.hillel.it.xipiv.model.entity;
 import java.util.*;
 
 public class User {
+	// Should I make fields private?
 	String userName;
+	// Do I need to store a password in the class?
 	UserGroup userGroup;
 	Calendar dateOfBirth;
-	List<User> followingUsers = new ArrayList<User>();
+	List<User> followingUsers = new ArrayList<User>(); // Should I use list or a simple array?
+	
+	// Which methods are going to be implemented here?
 }
